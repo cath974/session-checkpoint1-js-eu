addeb982d655134f4a988712c8097535931387b9
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-11-01 10:32:00 +0200

    algo3.js

--- algo3.js
+++ algo3.js
@@ -5,5 +5,17 @@
 */
 
 function fibonacci(num) {
+    let a = 0;
+    let b = 1;
+    let fNext = 0;
+    // let res = 0;
 
+    for (let i = 0; i < num -1; i ++) {
+        fNext = a + b
+        a = b
+        b = fNext
+    }
+    return b
 }
+
+console.log(fibonacci(10))
