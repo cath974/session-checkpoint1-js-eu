41d8fd3d7aebbdf6ca48331da17d51c39b5bc03c
Thaore <grascao@gmail.com>
2019-11-04 11:08:17 +0100

    exo2

--- algo2.js
+++ algo2.js
@@ -24,18 +24,21 @@ Fix the following function to help your new coworker!
 Note: try changing only the necessary code, don't rewrite the entire thing. 
 */
 
-var minMax = () => (array) => {
-    const min = 0;
-    const max = array[0];
+var minMax = (array) => {
+    let aRr = [];
+    let min = array[0];
+    let max = array[0];
 
-    for (let i = 1; i = array.length - 1; i + 1) {
-        if (array(i) < min) {
-            min = array;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
         }
-        if (array[i] = max) {
+        if (array[i] >= max) {
             max = array[i];
         }
-        return array;
     }
-    array.push(min, max);
+    aRr.push(min, max);
+    return aRr;
 }
+
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]))
