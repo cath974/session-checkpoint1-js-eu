39898ae55dc7b2778b08aee729c7e38fcb2dbb03
Deon Odendaal <deon.odendaal@protonmail.com>
2019-11-04 11:25:37 +0000

    algo3

--- algo3.js
+++ algo3.js
@@ -5,6 +5,21 @@ If the user enters a number that is above 60, your program should fail, and log 
 (Hint: Create an HTML UI to capture input and display the result of running you algorithm)
 */
 
-function minuteAngle(minute) {
+const button = document.getElementById("submit");
 
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  let minute = document.getElementById("minute").value;
+  let angle = minuteAngle(minute);
+  document.getElementById("result").innerHTML = `${angle} &#176`;
+});
+
+function minuteAngle(minute) {
+  if (minute < 0 || minute > 60) {
+    alert("Please enter a number between 0 and 60");
+  } else {
+    let separation = 360 / 60;
+    let angle = separation * minute;
+    return angle;
+  }
 }
